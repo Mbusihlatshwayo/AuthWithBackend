7d5ebcbccc089e4ae168bd6384bdd42c727dd0e5
Mbusi <mbusi@cox.net>
2019-06-02 22:12:28 -0400

    added function to remove x-auth token when logged out

--- Client/IrvineApartments/src/Screens/PrivateApartments.js
+++ Client/IrvineApartments/src/Screens/PrivateApartments.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Navigation } from 'react-native-navigation';
-import { Alert, StyleSheet, Text, TextInput, View, ImageBackground, Image, Button, TouchableHighlight, TouchableOpacity } from 'react-native';
-import bgImage from './Images/background.jpg'
-import logoImage from './Images/irvine-company-logo.png'
-import Validator from 'validator';
-import axios from 'axios';
+import { AsyncStorage ,Alert, StyleSheet, Text, TextInput, View, ImageBackground, Image, Button, TouchableHighlight, TouchableOpacity } from 'react-native';
 
 const authWidth = '85%';
 const authHeight = '65%';
@@ -12,11 +8,47 @@ const logoWidth = '70%';
 
 export default class App extends React.Component {
 
+    handleLogout = ()=>{
+        AsyncStorage.removeItem('x-auth').then(()=>{
+            alert('reload now');
+        }).catch(()=> {
+            alert('reload');
+        })
+    }
+
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text> YOU MADE IT TO OUR APARTMENTS </Text>
+                <TouchableOpacity 
+                    onPress = {
+                    this.handleLogout
+                    }
+                    style={styles.logoutButton}
+                    underlayColor='#ff0'>
+                    <Text style={styles.TextStyle}>SIGN OUT</Text>
+          </TouchableOpacity>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems: 'center',
+    },
+    logoutButton:{
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '90%',
+      height: 50,
+      backgroundColor:'#FFD700',
+    },
+    TextStyle: {
+        color: '#FFF',
+        fontSize: 30,
+        // fontStyle: 'bold',
+    },
+  });
+  
